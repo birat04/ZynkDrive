83695149053b940d10275bcd856a018459ed699c
Birat Roka <23btrcn071@jainuniversity.ac.in>
2026-03-17 15:05:41 +0530

    chore: remove .next from tracking

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"LRK3HyNQ+MJxt/ufSkhHheGsKmt+WKBEmvG3BKLTJ/g=\"\n}"
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ok4ISuAgU5agsJeIz1SYIo9d4jwffOXkgnSzJP6sPZw=\"\n}"
